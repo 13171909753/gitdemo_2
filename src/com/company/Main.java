97c1d56f138c111fd67b6ed0d9190fa6c6d2ea4a
刘艳星 <leonids801@163.com>
2023-08-31 16:13:51 +0800

    第一次提交哦

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         //第一次使用git提交
         System.out.println("hello world");
+        System.out.println("第一次提交git文件");
     }
 }
